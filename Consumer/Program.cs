78ed6809ca1012740ec71e0152db373f0270a843
Muhammad Firza Gustama <mfirza_gustama@bi.go.id>
2023-09-06 11:57:23 +0700

    producer and consumer

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Received new message: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Consuming");
+
+Console.ReadKey();
--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "This is my first message";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, body);
+
+Console.WriteLine($"Send message: {message}");
