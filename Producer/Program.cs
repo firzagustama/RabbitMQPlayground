fb6cdede78b9b399666ac81ce455dddc21d6f758
Muhammad Firza Gustama <mfirza_gustama@bi.go.id>
2023-09-06 14:34:53 +0700

    routing

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -6,11 +6,11 @@ var factory = new ConnectionFactory() { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.ExchangeDeclare(exchange: "pubsub", type: ExchangeType.Fanout);
+channel.ExchangeDeclare(exchange: "routing", type: ExchangeType.Direct);
 
-var message = "Hello I want to broadcast this message!";
+var message = "This message need to be routed";
 var body = Encoding.UTF8.GetBytes(message);
 
-channel.BasicPublish(exchange: "pubsub", "", null, body);
+channel.BasicPublish(exchange: "routing", "analyticsonly", null, body);
 
 Console.WriteLine($"Send message: {message}");
